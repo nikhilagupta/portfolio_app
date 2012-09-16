04413d9633998eb67732fcae91c585a62c8b1fbb
unknown <Administrator@celab2-16a.comp1-domain.spit.ac.in>
2012-09-16 15:16:15 +0530

    updated

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,2 +1,9 @@
 class Company < ActiveRecord::Base
+  validate :symbol_should_be_3_or_4_letters
+  def   symbol_should_be_3_or_4_letters
+  if symbol.length  >4  || symbol.length <3
+    errors.add :symbol,'symbol should be 3 or 4 characters'
+  end
+  end
+
 end
